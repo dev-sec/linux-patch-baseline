f983d53955c46b3b2fe5ad92fa12c6e30d3ccea2
Nandesh Guru <nandesh@cloudcoreo.com>
2017-11-02 16:24:51 -0700

    Remove puts from updates

--- libraries/linux_updates.rb
+++ libraries/linux_updates.rb
@@ -135,7 +135,6 @@ EOH
 #!/bin/sh
 python -c 'import sys; sys.path.insert(0, "/usr/share/yum-cli"); import cli; list = cli.YumBaseCli().returnPkgLists(["updates"]);res = ["{\\"name\\":\\""+x.name+"\\", \\"version\\":\\""+x.version+"-"+x.release+"\\",\\"arch\\":\\""+x.arch+"\\",\\"repository\\":\\""+x.repo.id+"\\"}" for x in list.updates]; print "{\\"available\\":["+",".join(res)+"]}"'
 EOH
-    puts rhel_updates
     cmd = @inspec.bash(rhel_updates)
     unless cmd.exit_status == 0
       # essentially we want https://github.com/chef/inspec/issues/1205
