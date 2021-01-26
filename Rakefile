60514f0a9de5b35579423f8f17b4b4d940d38db3
Martin Schurz <Martin.Schurz@t-systems.com>
2021-01-26 22:20:21 +0100

    update code to conform to new linting rules
    Signed-off-by: Martin Schurz <Martin.Schurz@t-systems.com>

--- Rakefile
+++ Rakefile
@@ -1,5 +1,6 @@
 #!/usr/bin/env rake
 # encoding: utf-8
+# frozen_string_literal: true
 
 require 'rake/testtask'
 require 'rubocop/rake_task'
--- controls/patches.rb
+++ controls/patches.rb
@@ -1,4 +1,6 @@
 # encoding: utf-8
+# frozen_string_literal: true
+
 # copyright: 2016, Christoph Hartmann
 # copyright: 2016, Dominik Richter
 # license: MPLv2
@@ -18,12 +20,12 @@ end
 control 'patches' do
   impact 0.3
   title 'All operating system package updates are installed'
-  linux_update.updates.each { |update|
+  linux_update.updates.each do |update|
     describe package(update['name']) do
       its('version') { should eq update['version'] }
     end
-  }
-  only_if { linux_update.updates.length > 0 }
+  end
+  only_if { linux_update.updates.length.positive? }
 end
 
 control 'os-patches' do
